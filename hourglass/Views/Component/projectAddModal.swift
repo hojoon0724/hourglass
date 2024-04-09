89561dfc3c7a45fb42f9175fbe080c2c9b8a6d50
hojoon0724 <hojoon0724@gmail.com>
2024-04-09 13:37:55 -0700

    project add modal form added, timer page with buttons

---
+++ hourglass/Views/Component/projectAddModal.swift
@@ -0,0 +1,18 @@
+//
+//  projectAddModal.swift
+//  hourglass
+//
+//  Created by Hojoon Kim on 4/9/24.
+//
+
+import SwiftUI
+
+struct projectAddModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    projectAddModal()
+}
