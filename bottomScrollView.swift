55337fa900f440132b5769c0b0013a1e810254ef
hojoon0724 <hojoon0724@gmail.com>
2024-04-09 18:12:14 -0700

    Timer page log layout done

---
+++ bottomScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  bottomScrollView.swift
+//  hourglass
+//
+//  Created by Hojoon Kim on 4/9/24.
+//
+
+import SwiftUI
+
+struct bottomScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    bottomScrollView()
+}
